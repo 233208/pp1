6aa7139fdae3d32d32fa1de1fd798ff6bbeb51bf
233208 <233208@student.uek.krakow.pl>
2023-10-11 11:03:03 +0200

    first commit

---
+++ 01-CourseIntroduction/zadania.py
@@ -0,0 +1,15 @@
+
+#x = input("Podaj swoje imie")
+#print("Witaj", x)
+#x=int(input("Podaj liczbę godzin"))
+#z=int(input("Podaj stawke godzinowa"))
+#print("Wynagrodzenie", x*z)
+width = 17
+height = 12.0
+#print(width//2) - int
+#print(width/2.0) - float
+#print(type(height/3)) - float
+#int
+#x = float(input("Podaj temp w C"))
+#print ((x*(9/5))+32)
+#
